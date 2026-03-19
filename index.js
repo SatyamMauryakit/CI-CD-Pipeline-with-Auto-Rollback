7b2224d83df028a2d2af5424c72700580b7d7b9d
SajtyamMauryakit <sa9451736205@gmail.com>
2026-03-20 01:58:10 +0530

    fix workflow -04

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("/health", (req, res) => {
-  res.status(404).send("Not Found");
+  res.status(200).send("OK");
 });
 
 app.listen(3000, () => console.log("Server running on port 3000"));
