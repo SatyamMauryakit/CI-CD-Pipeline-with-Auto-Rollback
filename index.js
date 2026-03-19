e7d2be1db26d6f61b5236fc4f247d23e49429549
SajtyamMauryakit <sa9451736205@gmail.com>
2026-03-20 00:52:51 +0530

    add index.html page

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("App is running 🚀");
+  res.sendFile(__dirname + "./index.html");
 });
 
 app.get("/health", (req, res) => {
